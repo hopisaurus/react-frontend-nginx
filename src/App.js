49cf8a1469d74e87d5faa332559b4e9c10104375
Andrew <hopisaurus@gmail.com>
2019-04-15 23:52:07 -0400

    Fixed to proper source of image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import pic from '../public/wavecheck.png'
 import './App.css';
 
 class App extends Component {
@@ -20,7 +19,7 @@ class App extends Component {
           >
             goaway
           </a>
-          <img src={pic} className="App-pic" alt="pic" />
+          <img src="/wavecheck.png" className="App-pic" alt="pic" />
         </header>
       </div>
     );
