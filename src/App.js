65ee98b385eb2d9fe95305f2954a0d50516dcf06
Andrew <hopisaurus@gmail.com>
2019-04-15 23:48:24 -0400

    Added pic for new site

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import pic from '../public/wavecheck.png'
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             goaway
           </a>
+          <img src={pic} className="App-pic" alt="pic" />
         </header>
       </div>
     );
